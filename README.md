1bfd9a8054bf11c1aba69131563e28db81d4568a
Beata Gyorgy <gybeacska19@gmail.com>
2023-01-05 19:07:39 +0100

    restaurant and review models

--- README.md
+++ README.md
@@ -22,3 +22,9 @@ Things you may want to cover:
 * Deployment instructions
 
 * ...
+
+rails g scaffold Restaurant name address phone_number category
+rails db:migrate
+
+rails generate model Review rating:integer content:text restaurant:references
+rails db:migrate
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'reviews/new'
+  get 'reviews/create'
+  resources :restaurants do
+    resources :reviews, only: [:new, :create]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
